ab349fcd2ee1e2e40e671bfee4dce09d06401486
Souranshu2410 <souranshu.sen@gmail.com>
2025-10-18 12:01:43 +0700

    Reversed array

---
+++ Main.py
@@ -0,0 +1,16 @@
+#Program to reverse the same array
+A = [1, 2, 3, 4, 5, 6]
+#Initialising start and end
+start = 0
+end = len(A)-1
+
+#Reverse A from start to end
+while start < end:
+    # Swapping the elements of an array to reverse it in the same array
+    A[start], A[end] = A[end], A[start]
+    start += 1
+    end -= 1
+
+#Driver Code
+print("Reversed array is")
+print(A)
